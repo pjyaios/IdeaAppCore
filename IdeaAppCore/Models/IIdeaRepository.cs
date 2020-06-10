f7de8bc1200b6b37690a2bac3088eb1dd2d7ec52
jypark <jypark@mworks.co.kr>
2020-06-10 18:03:46 +0900

    초기 커밋.

---
+++ IdeaAppCore/Models/IIdeaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdeaAppCore.Models
+{
+    public interface IIdeaRepository
+    {
+        Task<Idea> AddIdea(Idea idea);
+        Task<List<Idea>> GetIdeas();
+    }
+
+}
---
+++ IdeaAppCore/Models/Idea.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdeaAppCore.Models
+{
+    public class Idea
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Note { get; set; }
+    }
+
+}
